e238ebfa5378e6bf0c5870972d4ac1893bb6ad55
fnakhsan <fn.akhsan@gmail.com>
2024-10-01 17:11:12 +0700

    feat: Add list anime use cases

---
+++ app/src/main/java/com/ppl/miru/anime/domain/list/model/LastUpdateAnimeModel.kt
@@ -0,0 +1,9 @@
+package com.ppl.miru.anime.domain.list.model
+
+data class LastUpdateAnimeModel(
+    val id: String,
+    val episodeId: String,
+    val episodeNumber: Int,
+    val title: String,
+    val image: String,
+)
